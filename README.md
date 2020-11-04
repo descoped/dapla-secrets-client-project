8c101119dd14c511a40ad96067f71ebd45b60d09
oranheim <oranheim@gmail.com>
2020-11-04 16:55:22 +0100

    SecretManagerClient is a provider based API for reading secrets

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Dapla Secret Manager Client
 
+SecretManagerClient is a provider based API for reading secrets.
+
+Supported providers are:
+
+* Dynamic Secret Provider
+* Google Secret Manager Provider
+
+The Google SecretManager support service-account and compute-engine authentication.
+
+Usage:
+
+1) load configuration with property 'gcp.service-account.file'
+```
+DynamicConfiguration configuration = new StoreBasedDynamicConfiguration.Builder()
+        .propertiesResource("application-override.properties")
+        .build();
+```
+
+2) google secret manager specific provider configuration
+```
+Map<String, String> providerConfiguration = Map.of(
+        "secrets.provider", "google-secret-manager",
+        "secrets.projectId", "ssb-team-dapla",
+        "secrets.serviceAccountKeyPath", configuration.evaluateToString("gcp.service-account.file")
+);
+```
+
+3) create client and read secret
+```
+try (SecretManagerClient client = SecretManagerClient.create(providerConfiguration)) {
+    assertEquals("42\n", client.readString("AN_ANSWER"));
+    assertArrayEquals("42\n".getBytes(), client.readBytes("AN_ANSWER"));
+}
+```
+
+Please refer to `SecretManagerTest` for further details.
---
+++ src/test/java/no/ssb/dapla/secrets/api/SecretManagerTest.java
@@ -0,0 +1,56 @@
+package no.ssb.dapla.secrets.api;
+
+import no.ssb.config.DynamicConfiguration;
+import no.ssb.config.StoreBasedDynamicConfiguration;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecretManagerTest {
+
+    @Test
+    void readDynamicSecret() {
+        Map<String, String> providerConfiguration = Map.of(
+                "secrets.provider", "dynamic-secret-configuration",
+                "secrets.propertyResourcePath", "application-secret.properties"
+        );
+
+        try (SecretManagerClient client = SecretManagerClient.create(providerConfiguration)) {
+            assertEquals("42", client.readString("AN_ANSWER"));
+            assertArrayEquals("42".getBytes(), client.readBytes("AN_ANSWER"));
+        }
+    }
+
+//    @Disabled
+    @Test
+    void readGoogleSecret() {
+        DynamicConfiguration configuration = new StoreBasedDynamicConfiguration.Builder()
+                .propertiesResource("application-override.properties")
+                .build();
+
+        Map<String, String> providerConfiguration = Map.of(
+                "secrets.provider", "google-secret-manager",
+                "secrets.projectId", "ssb-team-dapla",
+                "secrets.serviceAccountKeyPath", getServiceAccountFile(configuration)
+        );
+
+        try (SecretManagerClient client = SecretManagerClient.create(providerConfiguration)) {
+            assertEquals("42\n", client.readString("AN_ANSWER"));
+            assertArrayEquals("42\n".getBytes(), client.readBytes("AN_ANSWER"));
+        }
+    }
+
+    String getServiceAccountFile(DynamicConfiguration configuration) {
+        String path = configuration.evaluateToString("gcp.service-account.file");
+        if (!Files.isReadable(Paths.get(path))) {
+            throw new RuntimeException("Missing 'application-override.properties'-file with required property 'gcp.service-account.file'");
+        }
+        return path;
+    }
+
+}
